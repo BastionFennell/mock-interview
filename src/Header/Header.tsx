1300f57087cfe967390813cc206e5d7bd9b2dfe4
Bastion Fennell <bfennell@alegion.com>
2020-04-10 15:10:53 -0500

    Clean up header styling

--- src/Header/Header.tsx
+++ src/Header/Header.tsx
@@ -2,8 +2,13 @@ import React from 'react';
 import styled from 'styled-components';
 
 const StyledHeader = styled.header`
+  align-items: center;
   background: #e2e2e2;
+  display: flex;
+  font-size: 20px;
+  font-weight: bold;
   height: 50px;
+  padding-left: 15px;
 `;
 
 const Header: React.SFC = () => <StyledHeader>Blerp Corp</StyledHeader>;
